f3508a7addb9a30c9e40e971d00a1ea26674c545
TW <vlad196593@gmail.com>
2025-07-11 00:11:12 +0300

    update v 1.6.1 new payments

--- shopadmin/settings.py
+++ shopadmin/settings.py
@@ -133,6 +133,7 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
+SITE_URL = 'https://yourdomain.com', 'http://localhost:8000'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
